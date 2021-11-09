4ae30b635d3e7b2877aba792d2fca2fa6cc3aa77
Opilane TTHK <opilane@tthk.ee>
2021-11-09 15:43:20 +0200

    first commit

---
+++ Horoscope/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace Horoscope
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your horoscope sign:");
+            string usersign = Console.ReadLine();
+            Console.WriteLine(Gethoroscope(usersign));
+            
+        }
+
+        public static string[] Readhoroscope()
+        {
+            string filepath = @"C:\Users\opilane\Samples\horoscope.txt";
+            string[] datafromfile = File.ReadAllLines(filepath);
+            
+            
+
+            return datafromfile;
+        } 
+        public static string Gethoroscope(string userinput)
+        {
+            string[] horoscope = Readhoroscope();
+            string todayhoroscope = "";
+            foreach(string element in horoscope)
+            {
+                if (element.Contains(userinput))
+                {
+                    todayhoroscope = element;
+                }   
+                
+            }
+            return todayhoroscope;
+        }
+
+    }
+}
---
+++ colours/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace colours
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your favourite colour:");
+            string usercolor = Console.ReadLine();
+            Console.WriteLine(Getfavouritecolor(usercolor));
+        }
+
+        public static string[] ReadColor()
+        {
+            string filepath = @"C:\Users\opilane\Samples\personality.txt";
+            string[] datafromfile = File.ReadAllLines(filepath);
+
+            return datafromfile;
+        }
+        public static string Getfavouritecolor(string userinput)
+        {
+            string[] colour = ReadColor();
+            string todaycolor = "";
+            foreach (string element in colour)
+            {
+                if (element.Contains(userinput))
+                {
+                    todaycolor  = element;
+                }
+
+            }
+            return todaycolor;
+        }
+
+
+
+    }
+}
+    
